b36edbbfaa56bf42cca4421edaf2738a656fcfb5
KyZoar <marion.pellicer@laposte.net>
2018-09-14 16:56:52 +0200

    Première partie du TP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'mon-pseudo-blog';
+
+  posts: Array<Post>=[
+    new Post('Mon premier post', 'S\'il y a plus de "like" alors le fond devient vert'),
+    new Post('Mon deuxième post', 'Dans le cas contraire, il devient rouge'),
+    new Post('Encore un post', 'Et en cas d\'égalite il est blanc')
+  ];
+
+}
+
+export class Post {
+  title: string;
+  content: string;
+  loveIts: number;
+  created_at: Date;
+
+  //Automatically assigns date
+  constructor(title: string, content: string) {
+    this.title = title;
+    this.content = content;
+    this.loveIts = 0;
+
+    this.created_at = new Date();
+  }
 }
---
+++ src/app/post-list-component/post-list-component.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../app.component';
+
+@Component({
+  selector: 'app-post-list-component',
+  templateUrl: './post-list-component.component.html',
+  styleUrls: ['./post-list-component.component.scss']
+})
+export class PostListComponentComponent implements OnInit {
+
+  @Input() postList: Array<Post>;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
